bd658042a9236515d00179ecad0ea759c881e1a0
Elkeid-me <elkeid.me@gmail.com>
2023-11-02 19:07:00 +0800

    Fix lab 1

--- kern/monitor.c
+++ kern/monitor.c
@@ -27,6 +27,7 @@ extern uint32_t fg_color, bg_color;
 static struct Command commands[] = {
     {"help", "Display this list of commands", mon_help},
     {"kerninfo", "Display information about the kernel", mon_kerninfo},
+        {"backtrace", "Backtrace the stack", mon_backtrace},
     {"color", "Change color", mon_color}};
 // clang-format off
 
