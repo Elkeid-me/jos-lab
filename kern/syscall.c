a52903dd7daa5dfce929a360107b73d944d8630c
Elkeid-me <elkeid.me@gmail.com>
2023-12-17 18:01:03 +0800

    Lab 5 Exercise 7

--- kern/syscall.c
+++ kern/syscall.c
@@ -130,13 +130,26 @@ static int sys_env_set_status(envid_t envid, int status)
 // Returns 0 on success, < 0 on error.  Errors are:
 //	-E_BAD_ENV if environment envid doesn't currently exist,
 //		or the caller doesn't have permission to change envid.
-static int
-sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
+static int sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 {
-	// LAB 5: Your code here.
-	// Remember to check whether the user has supplied us with a good
-	// address!
-	panic("sys_env_set_trapframe not implemented");
+    // LAB 5: Your code here.
+    // Remember to check whether the user has supplied us with a good
+    // address!
+    struct Env *env_ptr;
+    if (envid2env(envid, &env_ptr, 1) < 0)
+        return -E_BAD_ENV;
+    env_ptr->env_tf = *tf;
+
+    env_ptr->env_tf.tf_cs |= GD_UT | 3;
+    env_ptr->env_tf.tf_ss |= GD_UD | 3;
+    env_ptr->env_tf.tf_ds |= GD_UD | 3;
+    env_ptr->env_tf.tf_es |= GD_UD | 3;
+
+    env_ptr->env_tf.tf_eflags |= FL_IF;
+    env_ptr->env_tf.tf_eflags &= ~FL_IOPL_MASK;
+    env_ptr->env_tf.tf_eflags |= FL_IOPL_0;
+
+    return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -432,6 +445,8 @@ int32_t syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3,
         return sys_ipc_try_send(a1, a2, (void *)a3, a4);
     case SYS_ipc_recv:
         return sys_ipc_recv((void *)a1);
+    case SYS_env_set_trapframe:
+        return sys_env_set_trapframe(a1, (struct Trapframe *)a2);
     default:
         return -E_INVAL;
     }
